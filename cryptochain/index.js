feba100af2db5e9ffc6560b7b89292ddcad7cdbb
nishanthmhegde <=>
2020-10-08 20:40:18 +0530

    Added Express API and blockchain sync

---
+++ cryptochain/index.js
@@ -0,0 +1,52 @@
+const Block = require('./block');
+const Blockchain = require('./blockchain');
+const express = require('express');
+const bodyParser = require('body-parser');
+const PubSub = require('./pubsub');
+const request = require('request');
+
+const blockchain = new Blockchain();
+const pubSub = new PubSub({blockchain});
+const app = express();
+const DEFAULT_PORT = 3000;
+const DEFAULT_URL = `http://localhost:${DEFAULT_PORT}`;
+
+//code to sync the chains when peer port is used
+const syncChains = () =>{
+	request({url:`${DEFAULT_URL}/api/blocks`}, (error, response, body)=>{
+		if(!error && response.statusCode===200){
+			console.log("Replacing the chain with root chain");
+			const rootChain = JSON.parse(response.body);
+			blockchain.replace_chain(rootChain);
+		}
+	});
+};
+
+//Use the body-parser middleware to parse incoming JSON for PUT/POST requests.
+app.use(bodyParser.json());
+
+//get the blockchain
+app.get('/api/blocks', (req, res)=>{
+	res.json(blockchain.chain);
+});
+
+//mine the block
+app.post('/api/mine', (req,res)=>{
+	const {data} = req.body;
+	blockchain.add_block({data});
+	pubSub.publish('BLOCKCHAIN', JSON.stringify(blockchain.chain));
+	res.redirect('/api/blocks');
+});
+
+let PEER_PORT;
+if (process.env.PEER_PORT==='true'){
+	PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+app.listen(PORT, ()=>{
+	console.log(`Listening on port ${PORT}`);
+	if (PORT!==DEFAULT_PORT){
+		syncChains();
+	}
+});
---
+++ cryptochain/pubsub.js
@@ -0,0 +1,49 @@
+const redis = require('redis');
+
+const CHANNELS = {
+	BLOCKCHAIN: 'BLOCKCHAIN',
+	TEST: 'TEST'
+};
+class PubSub{
+	constructor({blockchain}){
+		this.publisher = redis.createClient();
+		this.subscriber = redis.createClient();
+		this.blockchain = blockchain;
+		this.subscribe();
+		this.subscriber.on('message', (channel, message) =>{
+			this.handle_message(channel, message);
+		});
+	}
+
+	handle_message(channel, message){
+		console.log(`Message ${message} received on channel ${channel}`);
+		if (channel ==='BLOCKCHAIN'){
+			const new_chain = JSON.parse(message);
+			this.blockchain.replace_chain(new_chain);
+			console.log("Replaced the chain");
+		}
+	}
+
+	subscribe(){
+		Object.values(CHANNELS).forEach((channel)=>{
+			this.subscriber.subscribe(channel);
+		});
+	}
+
+	publish(channel, message){
+		//subscriber needs to unsubscrible from the channel on which it is 
+		//publishing a message and then re-subscibe again to the same channel.
+		this.subscriber.unsubscribe(channel, ()=>{
+			this.publisher.publish(channel, message, ()=>{
+				this.subscriber.subscribe(channel);
+			});
+		});
+	}
+}
+
+// pubsub = new PubSub();
+// setTimeout(()=>{
+// 	pubsub.publish('TEST', "Hello World!");
+// }, 1000);
+
+module.exports = PubSub;
